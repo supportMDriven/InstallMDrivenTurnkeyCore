579baa669b834bd0ccd8a85aecb3587788c8e231
hanskarlsen <hans@karlsen.se>
2024-09-01 18:15:54 +0200

    New build

---
+++ js/mdriven.blazor.js
@@ -0,0 +1,36 @@
+﻿let _dotnethelper = null;
+
+function TheResizeCallback() {
+  if (_dotnethelper) {
+    _dotnethelper.invokeMethodAsync('OnResize', window.innerWidth, window.innerHeight);
+  }
+};
+
+window.registerViewportChangeCallback = (dotnetHelper) => {
+  _dotnethelper = dotnetHelper;
+  window.addEventListener('resize', TheResizeCallback);
+};
+
+window.unRegisterViewportChangeCallback = function () {
+  window.removeEventListener('resize', TheResizeCallback);
+  _dotnethelper = null;
+};
+
+
+window.getDimensions = function () {
+  return {
+    width: window.innerWidth,
+    height: window.innerHeight
+  };
+};
+
+window.getBoundingClientRectMDriven = function (elementId) {
+  let elem = document.getElementById(elementId);
+  if (elem != null)
+    return elem.getBoundingClientRect();
+  return null;
+};
+
+window.setCssVarMDriven = function (varname, varvalue) {
+  document.documentElement.style.setProperty(varname, varvalue);
+};
