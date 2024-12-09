63ea2a0fc05366db7b270bc06f4e7362218e5663
hanskarlsen <hans@karlsen.se>
2024-12-09 05:25:47 +0100

    New build

--- js/mdriven.cellselect.js
+++ js/mdriven.cellselect.js
@@ -3,13 +3,19 @@
 
 
 //////////CELL SELECT
-window.TableKeyDownMDriven = function (thetable) {
+window.TableKeyDownMDriven = function (thetable, angularscope) {
   thetable._cellAnchor = null;
   thetable._cellLast = null;
- 
+
   thetable.addEventListener('mousedown', function (event) {
-    if (event.target.tagName == 'TD') {
-      let thecurrentcell = event.target;
+    let correcttarget = event.target;
+    let isBlazor = false;
+    if (event.target.tagName == 'DIV' && event.target.parentElement.tagName == 'TD') {
+      correcttarget = event.target.parentElement; // blazor
+      isBlazor = true;
+    }
+    if (correcttarget.tagName == 'TD') {
+      let thecurrentcell = correcttarget;
       let thecurrentrow = thecurrentcell.parentElement;
       let row = thecurrentrow.rowIndex - 1; // correct for header
       let col = thecurrentcell.cellIndex;
@@ -17,14 +23,14 @@ window.TableKeyDownMDriven = function (thetable) {
       if (event.shiftKey) {
         thetable._cellLast = thecurrentcell;
       }
-      else { 
+      else {
         thetable._cellAnchor = thecurrentcell;
         thetable._cellLast = thecurrentcell;
 
       }
 
-      UpdateCellSelects(thetable);
-      thecurrentcell.focus();
+      UpdateCellSelects(thetable, isBlazor);
+      //thecurrentcell.focus();
       thetable._mousedown = true;
 
     }
@@ -36,10 +42,16 @@ window.TableKeyDownMDriven = function (thetable) {
 
 
   thetable.addEventListener('mousemove', function (event) {
-    if (event.target.tagName == 'TD' && thetable._mousedown == true) {
-      let thecurrentcell = event.target;
+    let correcttarget = event.target;
+    let isBlazor = false;
+    if (event.target.tagName == 'DIV' && event.target.parentElement.tagName == 'TD') {
+      correcttarget = event.target.parentElement; // blazor
+      isBlazor = true;
+    }
+    if (correcttarget.tagName == 'TD' && thetable._mousedown == true) {
+      let thecurrentcell = correcttarget;
       thetable._cellLast = thecurrentcell;
-      UpdateCellSelects(thetable);
+      UpdateCellSelects(thetable, isBlazor);
     }
   });
 
@@ -50,14 +62,17 @@ window.TableKeyDownMDriven = function (thetable) {
     const activeElement = document.activeElement;
     if (activeElement == null)
       return;
-      if (activeElement.tagName === 'TD' ) { 
-
+    let isBlazor = false;
+    let correcttarget = activeElement;
+    if (activeElement.tagName == 'DIV' && activeElement.parentElement.tagName == 'TD') {
+      correcttarget = activeElement.parentElement; // blazor
+      isBlazor = true;
     }
 
 
-    if (activeElement.tagName === 'TD' ) {
-      let thecurrentcell = activeElement;
-      let thecurrentrow = activeElement.parentElement;
+    if (correcttarget.tagName === 'TD') {
+      let thecurrentcell = correcttarget;
+      let thecurrentrow = correcttarget.parentElement;
       let row = thecurrentrow.rowIndex - 1; // correct for header
       let col = thecurrentcell.cellIndex;
 
@@ -74,12 +89,24 @@ window.TableKeyDownMDriven = function (thetable) {
         case 'ArrowRight':
           col = col < thecurrentrow.cells.length - 1 ? col + 1 : col;
           break;
+        case 'V', 'v':
+          if (event.ctrlKey) {
+            if (angularscope != null)
+              angularscope.$emit('cellPasteMDriven', null);
+          }
+          break;
+        case 'C', 'c':
+          if (event.ctrlKey) {
+              CopyDataToClipFromCellSelectMDriven(thetable, angularscope);
+          }
+          break;
         case 'Enter': {
           const input = thecurrentcell.querySelector('input');
           if (input) {
             input.focus();
+            return;
           }
-          return;
+          break;
         }
         default:
           return; // Quit when this doesn't handle the key event.
@@ -94,15 +121,82 @@ window.TableKeyDownMDriven = function (thetable) {
         thetable._cellAnchor = nextCell;
         thetable._cellLast = nextCell;
       }
-      UpdateCellSelects(thetable);
-      nextCell.focus();
+      UpdateCellSelects(thetable, isBlazor);
+      if (isBlazor)
+        nextCell.children[0].focus();
+      else
+        nextCell.focus();
       event.preventDefault();
     }
   });
 
 }
 
-function UpdateCellSelects(thetable) {
+function CopyDataToClipFromCellSelectMDriven(thetable, angularscope) {
+
+  if (thetable._lastAnchor == null)
+    return;
+  if (thetable._lastLast == null)
+    thetable._lastLast = thetable._lastAnchor;
+
+  const minRow = Math.min(thetable._lastAnchor.parentElement.rowIndex - 1, thetable._lastLast.parentElement.rowIndex - 1);
+  const maxRow = Math.max(thetable._lastAnchor.parentElement.rowIndex - 1, thetable._lastLast.parentElement.rowIndex - 1);
+  const minCol = Math.min(thetable._lastAnchor.cellIndex, thetable._lastLast.cellIndex);
+  const maxCol = Math.max(thetable._lastAnchor.cellIndex, thetable._lastLast.cellIndex);
+
+  let body = thetable._lastAnchor.parentElement.parentElement;
+  let isfirstrow = true;
+  let clipdata = '';
+  for (var i = minRow; i <= maxRow; i++) {
+    let cellelem = body.rows[i];
+    let rowhasdata = false;
+    let isfirst = true;
+    let rowdata = '';
+
+
+    if (!angularscope) {
+    }
+    else {
+      var headerrowelement = thetable.querySelector('thead').querySelector('tr');
+      let angularelement = angular.element(cellelem);
+      if (angularelement) {
+        let listitem = angularelement.scope().row;
+        for (var x = minCol; x <= maxCol; x++) {
+          let membername = headerrowelement.children[x].attributes.colname.value;
+          let memberitem = listitem[membername];
+          if (memberitem && memberitem != '')
+            rowhasdata = true;
+          if (!isfirst)
+            rowdata += '\t';
+          rowdata += memberitem;
+          isfirst = false;
+        }
+
+      }
+      if (rowhasdata) {
+        if (!isfirstrow)
+          clipdata += '\r\n';
+        clipdata += rowdata;
+        isfirstrow = false;
+      }
+
+
+    }
+
+  }
+
+  if (!angularscope) {
+
+  }
+  else {
+    angularscope.$parent.ViewData.RootVMClassObject["VM_Variables"]["vClipbookData"] = clipdata;
+    angularscope.$parent.$broadcast('__vClipbookDataChanged');
+
+  }
+}
+
+
+function UpdateCellSelects(thetable, isBlazor) {
 
   if (thetable._lastLast == null)
     thetable._lastLast = thetable._lastAnchor;
@@ -110,7 +204,13 @@ function UpdateCellSelects(thetable) {
   if (thetable._lastAnchor != null) {
     let square = getSquareSelection(thetable._lastAnchor, thetable._lastLast);
     let body = thetable._lastAnchor.parentElement.parentElement;
-    square.forEach(cell => { body.rows[cell.row].cells[cell.col].classList.remove('cellselect'); });
+    square.forEach(cell => {
+      let cellelem = body.rows[cell.row].cells[cell.col];
+      if (isBlazor)
+        cellelem.children[0].classList.remove('cellselect');
+      else
+        cellelem.classList.remove('cellselect');
+    });
     //thetable._lastAnchor.classList.remove('cellselect');
   }
 
@@ -121,7 +221,13 @@ function UpdateCellSelects(thetable) {
   if (thetable._cellAnchor != null) {
     let square = getSquareSelection(thetable._cellAnchor, thetable._cellLast);
     let body = thetable._cellAnchor.parentElement.parentElement;
-    square.forEach(cell => { body.rows[cell.row].cells[cell.col].classList.add('cellselect'); });
+    square.forEach(cell => {
+      let cellelem = body.rows[cell.row].cells[cell.col];
+      if (isBlazor)
+        cellelem.children[0].classList.add('cellselect');
+      else
+        cellelem.classList.add('cellselect');
+    });
   }
 
 
